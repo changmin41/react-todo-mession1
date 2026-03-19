e10def74e0c53b1d8ba918edb563bff478314670
개발자1 <cm41823@gmail.com>
2026-03-20 06:44:54 +0900

    로컬스토리지에 정보 저장 및 렌더링 성공

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 /**
  *
  * 할일 등록과 삭제 기능이 있는 간단한 투두리스트 앱입니다.
@@ -7,26 +7,55 @@ import { useState, useRef } from 'react'
  * - 할일 삭제, 초기 로드(앱 실행 시 localStorage에서 데이터 불러오기)
  * - 완료 체크
  */
+const storage = window.localStorage
+
+const setItem = (key, value) => {
+    try {
+        // 저장할 때는 항상 문자열로 바꿔서 저장해야 합니다.
+        storage.setItem(key, JSON.stringify(value))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const getItem = (key, defaultValue) => {
+    try {
+        const storedValue = storage.getItem(key)
+        if (storedValue) {
+            return JSON.parse(storedValue)
+        }
+        return defaultValue
+    } catch (e) {
+        console.log(e)
+        return defaultValue
+    }
+}
 
 function App() {
-    const [todos, setTodos] = useState([
-        { id: 1, todo: '할일1', completed: true },
-        { id: 2, todo: '할일2', completed: false },
-        { id: 3, todo: '할일3', completed: false },
-    ])
-
-    let lastId = useRef(4) //id값이 4부터 시작하도록 설정
+    const [todos, setTodos] = useState(getItem('my-todos', []))
+    let lastId = useRef(1) //id값이 1부터 시작
+    //스토리지에서 데이터 불러오기
+    useEffect(() => {
+        setItem('my-todos', todos)
+    }, [todos])
 
     //할일 등록 함수
     function addTodo(e) {
         e.preventDefault() //새로고침 방지
+        const value = e.target.todo.value
         console.log(e.target.todo.value) //입력한 value값을 콘솔에 출력
-        setTodos([...todos, { todo: e.target.todo.value, completed: false }]) //todos 배열에 input의 value값을 추가
-        e.target.todo.value = '' // 입력창 지우기
-        return
 
-        setTodos([...todos, { id: lastId.current, todo: form.todo.value, completed: false }])
-        lastId.current++
+        if (!value.trim()) {
+            //빈값 입력 방지
+            alert('할 일을 입력해주세요!')
+            return
+        }
+
+        setTodos([...todos, { id: lastId.current, todo: value, completed: false }]) //todos 배열에 input의 value값을 추가
+        lastId.current++ // 다음 아이디 준비
+        e.target.todo.value = '' // 입력창 지우기
+
+        return
     }
 
     //할일 삭제 함수
