46ba9290cee41ceeeab563617681ad36ff5efbf6
개발자1 <cm41823@gmail.com>
2026-03-20 05:24:18 +0900

    할일 등록 환료

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,24 @@ import { useState } from 'react'
 
 function App() {
     const [todos, setTodos] = useState(['할일1', '할일2', '할일3'])
+
+    //할일 등록 함수
+    function addTodo(e) {
+        e.preventDefault() //새로고침 방지
+        console.log(e.target.todo.value) //입력한 value값을 콘솔에 출력
+        setTodos([...todos, e.target.todo.value]) //todos 배열에 input의 value값을 추가
+        return
+    }
+
     return (
         <>
+            <form onSubmit={addTodo}>
+                <input type="text" name="todo" placeholder="할일을 입력하세요" />
+                <button type="submit">추가</button>
+            </form>
             <ul>
                 {todos.map((item, i) => (
-                    <li> {item} </li>
+                    <li key={i}> {item} </li> //key 값으로 오류 해결
                 ))}
             </ul>
         </>
